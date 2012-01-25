ec91c749cf318d98ba274a6322edb805f5ae9aaa
Roman Timashev <roman@tmshv.ru>
2012-01-26 03:52:45 +0400

    archdaily-loadimgurls.py is ready to run

--- spiders/archdaily/archdaily-loadimgurls.py
+++ spiders/archdaily/archdaily-loadimgurls.py
@@ -7,9 +7,32 @@ def load_res(url):
 	res = urllib2.urlopen(url)
 	return res.read()
 
-def get_post_images(page):
+def get_post_images(page_url):
+	page = load_res(page_url)
 	html = lxml.html.fromstring(page)
-	imgs = html.xpath('.//*[@class="content"]//p/a/img/@src')
+	img_smalls = html.xpath('.//div[@class="post_content"]/div/a/img/@src')
+	img_pages = html.xpath('.//div[@class="post_content"]/div/a/@href')
+	img_bigs = get_big_image_url_list(img_pages)
+
+	images= []
+	for x in xrange(len(img_pages)):
+		images.append({'small': img_smalls[x], 'big': img_bigs[x], 'page': page_url})
+	return images
+
+def get_big_image_url_list(page_url_list):
+	imgs = []
+	for x in xrange(len(page_url_list)):
+		page_url = page_url_list[x]
+		page = load_res(page_url)
+		html = lxml.html.fromstring(page)
+		print 'get big from', page_url
+		try:
+			img = html.xpath('.//*[@id="download_image"]/a/@href')[0]
+			imgs.append(img)
+		except IndexError, e:
+			print 'ALERT: load big image warning:', page_url
+			imgs.append(page_url)
+		
 	return imgs
 
 ufile = open('urls.txt', 'r')
@@ -18,18 +41,15 @@ ufile.close()
 
 imgs = []
 for x in xrange(len(posts)):
-	print 'analyze', x
 	url = posts[x]['url']
-	page = load_res(url)
-	i = get_post_images(page)
+	print 'analyze page index', x, url
+	i = get_post_images(url)
 	imgs = imgs + i
+
+print 'compeled'
+print len(imgs)
+print imgs
 
 imgfile = open('imgs.txt', 'w')
 imgfile.write(json.dumps(imgs))
 imgfile.close()
-
-	# for x in xrange(len(imgs)):
-	# 	img = load_res(imgs[x])
-	# 	f = open('/Users/tmshv/Desktop/architizer/a'+str(x), 'wb')
-	# 	f.write(img)
-	# 	f.close()
